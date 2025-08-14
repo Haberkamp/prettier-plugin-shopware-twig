2ff67d407cea763a7a8afafc15fc4e93c7f3da46
Nils Haberkamp <haberkamp.n@gmail.com>
2025-08-14 18:39:04 +0200

    test: format classes with double quotes

---
+++ tests/html/attributes/double-quotes.test.ts
@@ -0,0 +1,18 @@
+import { it, expect } from "vitest";
+import * as prettier from "prettier";
+
+it("formats double quotes correctly", async () => {
+  // ARRANGE
+  const code = `<img src="test.png" alt="John 'ShotGun' Nelson">`;
+
+  // ACT
+  const result = await prettier.format(code, {
+    parser: "shopware-twig",
+    plugins: ["./src/index.js"],
+  });
+
+  // ASSERT
+  expect(result).toMatchInlineSnapshot(
+    `"<img src="test.png" alt="John 'ShotGun' Nelson" />"`
+  );
+});
